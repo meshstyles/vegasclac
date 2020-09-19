7eec3748a2b551eda1fb50f2a3d449605a27d6da
meshstyles <meshstyles@users.noreply.github.com>
2020-09-19 02:42:42 +0200

    hex rgb working

--- src/render.js
+++ src/render.js
@@ -1,10 +1,62 @@
+const submitBtn = document.getElementById("submit");
+submitBtn.onclick = color_coverter;
 
-function getColor() {
 
-    let __in_col_dec_r = form_col_dec_r.value;
-    // __form_col_dec_r.value = '255';
-    console.log(__in_col_dec_r);
+function color_coverter() {
+    let col_hex_rgb = form_col_hex_rgb.value;
 
-    document.getElementById("dec_col_output").innerHTML = __in_col_dec_r;
-    dec_col_res.style.display = "block";
+    let col_dec_r = form_col_dec_r.value;
+    let col_dec_g = form_col_dec_g.value;
+    let col_dec_b = form_col_dec_b.value;
+
+    let col_float_r = form_col_float_r.value;
+    let col_float_g = form_col_float_g.value;
+    let col_float_b = form_col_float_b.value;
+
+    let hexres, decres, floatres;
+
+    if (col_hex_rgb) {
+        hexres = col_hex_rgb;
+        decres = hex2dec(hexres);
+        floatres = dec2float(decres);
+    } else if (col_dec_r && col_dec_g && col_dec_b) {
+
+    } else if (col_float_r && col_float_g && col_float_b) {
+
+    }
+
+    document.getElementById("hex_color_res").innerHTML = `hex #${hexres}`;
+    document.getElementById("dec_color_res").innerHTML = `${decres[0]}, ${decres[1]}, ${decres[2]}, 255`;
+    document.getElementById("float_color_res").innerHTML = `${floatres[0]}, ${floatres[1]}, ${floatres[2]}, 255`;
+    document.getElementById("color_square").style.backgroundColor = `#${hexres}`
+    color_result.style.display = "block";
+}
+
+
+function hex2dec(hexres) {
+    let decres = [];
+    decres.push(parseInt(hexres.substring(0, 2), 16));
+    decres.push(parseInt(hexres.substring(2, 4), 16));
+    decres.push(parseInt(hexres.substring(4, 6), 16));
+    return decres;
+}
+
+function dec2float(decres) {
+    let floatres = [];
+    floatres.push((decres[0] / 255).toFixed(3).replace('.', ','));
+    floatres.push((decres[1] / 255).toFixed(3).replace('.', ','));
+    floatres.push((decres[2] / 255).toFixed(3).replace('.', ','));
+    return floatres;
+}
+
+function dec2hex(decres) {
+    return `${decres[0].toString(16)}${decres[1].toString(16)}${decres[2].toString(16)}`;
+}
+
+function float2dec(floatres) {
+    let decres = [];
+    decres.push((floatres[0] * 255).toFixed(0));
+    decres.push((floatres[1] * 255).toFixed(0));
+    decres.push((floatres[2] * 255).toFixed(0));
+    return decres;
 }
